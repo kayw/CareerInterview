ffd6f1771f45122b1e01f7d0e7dba1dac1b0e2ef
kayw <kayw@outlook.com>
2016-03-05 11:27:23 +0800

    knockout return result instead of pass args

--- july100_36_merge_knockout/knockout.rs
+++ july100_36_merge_knockout/knockout.rs
@@ -1,6 +1,7 @@
-fn knock_out(winners: &Vec<Vec<i32>>, orders: &Vec<i32>, results: &mut Vec<i32>) {
+fn knock_out(winners: &Vec<Vec<i32>>, orders: &Vec<i32>) -> Vec<i32>{
     let mut round = orders.len();
-    results.extend(orders.iter().cloned());
+    //results.extend(orders.iter().cloned());
+    let mut results = orders.clone();
     while round > 1 {
         let mut i = 0;
         let mut j = 0usize;
@@ -12,14 +13,14 @@ fn knock_out(winners: &Vec<Vec<i32>>, orders: &Vec<i32>, results: &mut Vec<i32>)
         }
         round /= 2;
     }
+    results
 }
 
 #[test]
 fn test_knock_out() {
     let winners = vec![vec![0,0,2,0], vec![0,1,2,3],vec![2,1,2,3], vec![0,3,3,3]];
     let orders = vec![3,1,0,2];
-    let mut results = vec![];
-    knock_out(&winners, &orders, &mut results);
+    let results = knock_out(&winners, &orders);
     assert_eq!(results[0], 3);
     assert_eq!(results[1], 2);
 }
@@ -29,8 +30,7 @@ fn test_knock_out_csdn_ylf13() {
     //http://blog.csdn.net/ylf13/article/details/13627263
     let winners = vec![vec![0,1,0,3], vec![1,1,2,1],vec![0,2,2,2], vec![3,1,2,3]];
     let orders = vec![2,0,1,3];
-    let mut results = Vec::new();
-    knock_out(&winners, &orders, &mut results);
+    let results = knock_out(&winners, &orders);
     assert_eq!(results[0], 1);
     assert_eq!(results[1], 0);
 }
