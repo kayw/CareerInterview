8d1c1733cca2b546bc4a63519e6fc7f42723045e
kayw <kayw@outlook.com>
2016-04-09 14:12:48 +0800

    fix zigzag optimization

--- leetcode/6_zigzag/converion.go
+++ leetcode/6_zigzag/converion.go
@@ -1,12 +1,11 @@
 package career_interview
 
 func Convert(s string, numRows int) string {
+	if numRows >= len(s) || numRows == 1 {
+		return s
+	}
 	buffer := make([]byte, len(s))
 	bufLen := 0
-	if numRows == 1 {
-		bufLen += copy(buffer[bufLen:], s)
-		return string(buffer)
-	}
 	//var index int
 	for r := 0; r < numRows; r++ {
 		j := 0
@@ -46,3 +45,17 @@ func Convert(s string, numRows int) string {
 	}
 	return string(buffer)
 }
+
+//https://leetcode.com/discuss/55208/if-you-are-confused-with-zigzag-pattern-come-and-see
+/*
+/*n=numRows
+Δ=2n-2    1                           2n-1                         4n-3
+Δ=        2                     2n-2  2n                    4n-4   4n-2
+Δ=        3               2n-3        2n+1              4n-5       .
+Δ=        .           .               .               .            .
+Δ=        .       n+2                 .           3n               .
+Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+Δ=2n-2    n                           3n-2                         5n-4
+
+that's the zigzag pattern the question asked! Be careful with nR=1 && nR=2
+*/
